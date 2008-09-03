200473d16438833d7c75e49f78bd0beb28e38638
Dmitry Karasik <dmitry@karasik.eu.org>
2008-09-03 07:03:02 +0000

    Import of KARASIK/Image-Match-1.02 from CPAN.
    gitpan-cpan-distribution: Image-Match
    gitpan-cpan-version:      1.02
    gitpan-cpan-path:         KARASIK/Image-Match-1.02.tar.gz
    gitpan-cpan-author:       KARASIK
    gitpan-cpan-maturity:     released

--- eg/test.pl
+++ eg/test.pl
@@ -1,18 +1,49 @@
-# $Id: test.pl,v 1.1 2008/09/01 09:26:28 dk Exp $
+# $Id: test.pl,v 1.2 2008/09/02 10:31:53 dk Exp $
 use strict;
 use Image::Match;
 
+$Image::Match::DEBUG++;
+
 # make screenshot
 my $big = Image::Match-> screenshot;
-# extract 50x50 image
-my $small = $big-> extract( 230, $big-> height - 70 - 230, 70, 70);
-# save
-$small-> save('1.png');
-# load
-$small = Prima::Image-> load('1.png') or die "Can't load: $@";
+# extract 70x70 image
+my $small = $big-> extract( 230, 230, 70, 70);
 # find again
-my ( $x, $y) = $big-> match( $small);
-print defined($x) ? "found at $x:$y\n" : "not found\n";
+my @all_matches = $big-> match( $small, 
+	multiple => 1,
+	mode     => 'geom',
+);
+
+# draw
+if ( @all_matches) {
+	$big-> begin_paint;
+	$big-> lineWidth(2);
+	my @sz = $small-> size;
+	for ( my $i = 0; $i < @all_matches; $i+=2) {
+		my ( $x, $y) = @all_matches[$i,$i+1];
+		print "found at: $x, $y\n";
+		$big-> rectangle( $x, $y, $x + $sz[0], $y + $sz[1]);
+	}
+	$big-> end_paint;
+} else {
+	print "not found\n";
+	exit;
+}
+
+
+# display
+use Prima qw(ImageViewer);
+my $w = Prima::MainWindow-> new( size => [ 640, 480 ]);
+$w-> insert( ImageViewer =>
+	pack => { fill => 'both', expand => 1 },
+	image => $big,
+);
+
+my $x = Prima::Window-> new;
+$x-> insert( ImageViewer =>
+	pack => { fill => 'both', expand => 1 },
+	image => $small,
+);
 
 run Prima;
 1;
--- lib/Image/Match.pm
+++ lib/Image/Match.pm
@@ -1,4 +1,4 @@
-# $Id: Match.pm,v 1.1 2008/09/01 09:26:28 dk Exp $
+# $Id: Match.pm,v 1.4 2008/09/02 10:34:44 dk Exp $
 package Image::Match;
 
 use strict;
@@ -7,12 +7,21 @@ use Prima::noX11;
 use Prima;
 require Exporter;
 
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 our $Y_GROWS_UPWARDS = 0;
+our $DEBUG = 0;
 
 sub match
 {
-        my ( $image, $subimage, $multiple) = @_;
+        my ( $image, $subimage, %options) = @_;
+
+	local $Y_GROWS_UPWARDS = $Y_GROWS_UPWARDS;
+	mode($options{mode}) if exists $options{mode};
+
+	$options{overlap} ||= 'some';
+	die "Bad overlap mode '$options{overlap}': must be one of: none, some, all\n"
+		unless $options{overlap} =~ /^(some|all|none)$/;
+	my $overlap_all = $options{overlap} eq 'all';
 
         my $G   = $image-> data;
         my $W   = $image-> width;
@@ -20,20 +29,31 @@ sub match
         my $w   = $subimage-> width;
         my $h   = $subimage-> height;
         my $bpp = ($image-> type & im::BPP) / 8;
+	print STDERR 
+		"match image ($w x $h x ",
+		$subimage-> type & im::BPP, ") in ", 
+		"($W x $H x ", 
+		$image-> type & im::BPP, ") ", 
+		"length ", length($G), "\n"
+		if $DEBUG;
 
-	# 1 and 4 bit images aren't supported, autoconvert
-	if ( $bpp < 0) {
-		$image = $image-> dup;
-		my $t = $subimage-> type & im::BPP;
-		$t = 8 if $t < 8;
-		$image-> type($t);
-        	$bpp = ($image-> type & im::BPP) / 8;
-	}
-
-	# need same bpp!
-	if ( $subimage-> type != $image-> type) {
-		$subimage = $subimage-> dup;
-		$subimage-> type( $image-> type);
+	# Requirements: need same bpp and same colormap.
+	# Also, 1 and 4 bit images aren't supported, autoconvert
+	if ( $bpp <= 1) {
+		my $cm1 = join(',', $image->    colormap);
+		my $cm2 = join(',', $subimage-> colormap);
+		if ( $cm1 eq $cm2) {
+			# good, palettes are equal. now, are types equal?
+			if ( $subimage-> type != $image-> type) {
+				$subimage-> type( $image-> type);
+				printf STDERR ("subimage converted to type=%x\n", $image->type) if $DEBUG;
+			}
+		} else {
+			# force convert to 24bits
+			$image-> type(24);
+			$subimage-> type(24);
+			print STDERR "both images converted to 24 bpp\n" if $DEBUG;
+		}
 	}
 
         my $I   = $subimage-> data;
@@ -41,43 +61,80 @@ sub match
         my $iw  = int(( $w * ( $subimage-> type & im::BPP) + 31) / 32) * 4;
         my $ibw = $w * $bpp;
         my $dw  = $gw - $ibw;
+	print "global=$gw, local=$iw, max=$ibw diff=$dw\n" if $DEBUG;
         
         my $rx  = join( ".{$dw}", map { quotemeta substr( $I, $_ * $iw, $ibw) } 
                 (0 .. $subimage-> height - 1));
         my ( $x, $y);
 	my @ret;
-	my $blanker = ("\0" x ( $bpp * $w));
 
+	pos($G) = 0;
+  	study $G;
 	while ( 1) {
-		pos($G) = 0;
-  		study $G;
-		my @loc_ret;
-		while ( 1) {
-		        unless ( $G =~ m/\G.*?$rx/gcs) {
-				return unless $multiple;
-				last;
-			}
-			my $p = pos($G);
-			$x = ($p - $w * $bpp) % $gw / $bpp;
-			$y = int(($p - ( $x + $w) * $bpp) / $gw) + 1;
-			next if $x + $w > $W; # scanline wrap
-			$y = $y - $h;
-			$y = $H - $h - $y unless $Y_GROWS_UPWARDS;
-        		push @loc_ret, [ $x, $y ];
-			return @{ $loc_ret[0] } unless $multiple;
+		if ( $DEBUG) {
+			my $ap = pos($G);
+			my $ax = $ap % $gw / $bpp;
+			my $ay = int(($ap - ($ax + $w) * $bpp) / $gw);
+			$ay = $H - $ay - 1 if $Y_GROWS_UPWARDS;
+			print STDERR 
+				"begin match at $ap = $ax $ay, ",
+				length($G) - $ap, " bytes left\n";
 		}
-		# blank zeros over the found stuff to avoid overlapping matches
-		for ( @loc_ret) {
-			my ( $x, $y) = @$_;
-			my $pos = $y * $gw + $x;
-			for ( my $i = 0; $i < $h; $i++, $pos += $gw) {
-				substr( $G, $pos, $w * $bpp) = $blanker;
-			}
+
+		# match
+	        unless ( $G =~ m/\G.*?$rx/gcs) {
+			print STDERR "-> negative\n" if $DEBUG;
+			return unless $options{multiple};
+			last;
 		}
-		push @ret, @loc_ret;
-		return @ret unless @loc_ret;
-		@loc_ret = ();
+		my $p = pos($G);
+		$x = ($p - $w * $bpp) % $gw / $bpp;
+		$y = int(($p - ( $x + $w) * $bpp) / $gw) + 1;
+		$y = $y - $h;
+		$y = $H - $h - $y unless $Y_GROWS_UPWARDS;
+		print STDERR "-> positive at $p = $x $y\n" if $DEBUG;
+
+		if ( $x + $w > $W) {
+			print STDERR "-> scanline wrap, skipping\n" if $DEBUG;
+			next;
+		}
+		pos($G) -= ($h - 1) * $gw;
+		pos($G) -= $ibw - $bpp if $overlap_all;
+
+		# store results
+		push @ret, $x, $y;
+		return $x, $y unless $options{multiple};
 	}
+
+	# filter output
+	if ( $options{overlap} eq 'none') {
+		my @r;
+		my @ranges; # for each scanline store list of occupied pixels as x1-x2 ranges
+		print STDERR "removing overlapped rectangles\n" if $DEBUG;
+		RECT: for ( my $i = 0; $i < @ret; $i+=2) {
+			my ( $x1, $y1) = @ret[$i, $i+1];
+			my ( $x2, $y2) = ( $x1 + $w, $y1 + $h);
+			print STDERR "checking ($x1,$y1)-($x2,$y2)\n" if $DEBUG;
+			for ( my $y = $y1; $y < $y2; $y++) {
+				$ranges[$y] ||= [];
+				for my $xranges ( @{ $ranges[$y] }) {
+					next if 
+						$x1 >= $xranges->[1] or
+						$x2 <  $xranges->[0];
+					print STDERR "-> overlaps, skipping\n" if $DEBUG;
+					next RECT;
+				}
+
+				# does not overlap, register
+				push @{ $ranges[$y] }, [ $x1, $x2 ];
+			}
+			push @r, $x1, $y1;
+		}
+		@ret = @r;
+	}
+
+	print STDERR "return: [@ret]\n" if $DEBUG;
+	return @ret;
 }
 
 sub screenshot
@@ -104,6 +161,15 @@ sub screenshot
 	return $::application-> get_image( $x, $y, $w, $h);
 }
 
+sub mode
+{
+	shift if defined($_[0]) and ( ref($_[0]) or ($_[0] =~ /Image/) );
+	return $Y_GROWS_UPWARDS ? 'geom' : 'screen' unless @_;
+	die "bad Image::Match::mode: must be 'geom' or 'screen'\n"
+		unless $_[0] =~ /^(geom|screen)$/;
+	$Y_GROWS_UPWARDS = $_[0] eq 'geom';
+}
+
 *Prima::Image::match = \&match;
 *Prima::Image::screenshot = \&screenshot;
 
@@ -113,19 +179,19 @@ sub screenshot
 
 =head1 NAME
 
-Image::Match - locate image inside another
+Image::Match - locate an image inside another
 
 =head1 DESCRIPTION
 
 The module searches for occurencies of an image inside of a larger image.
 
-The interesting stuff here is the image finding itself - it is done by a
-regexp!  For all practical reasons, images can be easily treated as byte
-strings, and regexps are not exception. For example, one needs to locate an
-image 2x2 in larger 7x7 image. The regexp constructed should be the first
-scanline of smaller image, 2 bytes, verbatim, then 7 - 2 = 5 of any character,
-and finally the second scanline, 2 bytes again. Of course there are some quirks,
-but these explained in API section.
+The interesting stuff here is the image finding itself - it is done by a regex!
+For all practical reasons, regexes operate on strings of bytes, and images can
+be easily treated as such. For example, one needs to locate an image 2x2 in a
+larger 7x7 image. The constructed regex should contain the first scanline of
+the smaller image, 2 bytes, verbatim, then match 7 - 2 = 5 of any byte found,
+and finally the second scanline, 2 bytes again. Of course there are some
+quirks, but these explained in the API section.
 
 The original idea was implemented in L<OCR::Naive> and L<Win32::GUIRobot>, but
 this module extracts the pure matching logic, unburdened from wrappers that
@@ -138,7 +204,7 @@ were needed back then for matters at hand.
 
   # make screenshot
   my $big = Image::Match-> screenshot;
-  # extract 50x50 image
+  # extract 70x70 image
   my $small = $big-> extract( 230, $big-> height - 70 - 230, 70, 70);
   # save
   $small-> save('1.png');
@@ -152,48 +218,61 @@ were needed back then for matters at hand.
 
 =over
 
-=item match $IMAGE, $SUBIMAGE, $MULTIPLE
+=item match $IMAGE, $SUBIMAGE, %OPTIONS
 
-Locates a $SUBIMAGE in $IMAGE, returns one or many matches, depending on $MULTIPLE.
-If single match is requested, stops on the first match, and returns a pair of (X,Y)
-coordinates. If $MULTIPLE is 1, returns array of (X,Y) pairs. In both modes, returns
-empty list if nothing was found.
+Locates a $SUBIMAGE in $IMAGE, returns one or many matches, depending on
+C<$OPTIONS{multiple}>.  If single match is requested, stops on the first match,
+and returns a single pair of (X,Y) coordinates. If C<$OPTIONS{multiple}> is 1,
+returns array of (X,Y) pairs. In both modes, returns empty list if nothing was
+found.
+
+C<$OPTIONS{mode}> overrides global C<mode()>.
+
+C<$OPTIONS{overlap}> can be set to one of three values: I<none>, I<some>,
+I<all>, to determine how the overlapping matches are reported when
+C<$OPTIONS{multiple}> is set.  I<None> will never report overlapping rectanges,
+and I<all> report all possible occurencies of C<$SUBIMAGE> in C<$IMAGE>.
+I<some> is similar to I<all>, but is a bit faster, and will not report
+overlapping rectangles that begin on the same scanline. I<some> is also the
+default overlapping mode.
 
 =item screenshot [ $X = 0, $Y = 0, $W = screen width, $H = screen height ]
 
 Returns a new C<Prima::Image> object with a screen shot, taken at
 given coordinates.
 
-=item $Y_GROWS_UPWARDS = 0
+=item mode $MODE = 'screen'
 
-The module uses L<Prima> for imaging storage and manipulations. Note that
-C<Prima>'s notion of graphic coordinates is such that Y axis grows upwards.
-This module can use both mathematical (Y grows upwards) and screen-based (Y
-grows downwards) modes.  The latter is default; set
-C<Image::Match::Y_GROWS_UPWARDS> to 1 to change that.
+The module uses L<Prima> for imaging storage and manipulations. Note that Prima
+uses coordinate system where Y axis grows upwards. This module however can use
+both geometrical (Y grows upwards, C<mode('geom')>) and screen-based (Y grows
+downwards, C<mode('screen')>) modes. The latter mode is the default.
 
 =back
 
 =head1 NOTES
 
-C<Prima> by default will start X11 session on unix. The module changes that
-behavior. If your code needs X11 connection, change that by explicitly stating
+On unix, C<Prima> by default will start X11. The module changes that behavior,
+so X11 connection is not needed. If your code though needs X11 connection, 
+change that by adding
 
    use Prima;
 
 before invoking
 
-   use Image::Match.
+   use Image::Match
 
-See L<Prima/noX11> for more.
+See L<Prima::X11> for more information.
 
-=head1 PREREQUISITES
-
-L<Prima>
+If you need to use other image backends than Prima, that can be done too.
+There is L<Prima::Image::Magick> that brings together Prima and ImageMagick,
+and there is L<Prima::Image::PDL>, that does the same for PDL. GD, Imglib2, and
+Imager, those we can't deal much with, except for saving to and loading from png
+files.
 
 =head1 SEE ALSO
 
-L<Prima>, L<OCR::Naive>, L<Win32::GUIRobot>
+L<Prima::Image>, L<OCR::Naive>, L<Win32::GUIRobot>
 
 =head1 LICENSE AND COPYRIGHT
 
